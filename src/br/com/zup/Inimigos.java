b9ce9f6daa6b35160452490e455a495867bd7d9a
Arê Nascimento <aretuza.gab@gmail.com>
2021-09-23 13:34:14 -0300

    classe inimigos

--- src/br/com/zup/Inimigos.java
+++ src/br/com/zup/Inimigos.java
@@ -1,4 +1,42 @@
 package br.com.zup;
 
 public class Inimigos {
+    private String nome;
+    private double vida;
+
+    public Inimigos (String nome, double vida) {
+        this.nome = nome;
+        this.vida = vida;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public double getVida() {
+        return vida;
+    }
+
+    public void setVida(double vida) {
+        this.vida = vida;
+    }
+
+    public void trocarNome() {
+        String nome = this.getNome();
+        this.setNome(nome);
+    }
+
+    public void receberDano(double dano) {
+        double recebeDano = this.getVida() - dano;
+        this.setVida(dano);
+    }
+
+    public void exibirVida() {
+        double vida = this.getVida();
+
+    }
 }
