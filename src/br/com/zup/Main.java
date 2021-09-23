736d2cd1a6ca37987fe9620f3547565f4ae5c879
Arê Nascimento <aretuza.gab@gmail.com>
2021-09-23 13:48:24 -0300

    Classe Rasteiros

--- src/br/com/zup/Main.java
+++ src/br/com/zup/Main.java
@@ -3,6 +3,16 @@ package br.com.zup;
 public class Main {
 
     public static void main(String[] args) {
-	// write your code here
+        Guerreiros warrior1 = new Guerreiros("Kezihakh Agrofite ", 500);
+        Inimigos enemy1 = new Inimigos("Kuoar Regozor", 550);
+
+        System.out.println("_________Welcome to the final batle_________");
+        System.out.println("Warrior: ");
+        System.out.println("____X____");
+        System.out.println(warrior1.getNome());
+        System.out.println("______________VERSUS______________");
+        System.out.println("Warrior:");
+        System.out.println("____X____");
+        System.out.println(enemy1.getNome());
     }
 }
---
+++ src/br/com/zup/Rasteiros.java
@@ -0,0 +1,10 @@
+package br.com.zup;
+
+public class Rasteiros extends Inimigos {
+    private double danoEspinhos;
+
+    public Rasteiros(String nome, double vida) {
+        super(nome, vida);
+        this.danoEspinhos = danoEspinhos;
+    }
+}
