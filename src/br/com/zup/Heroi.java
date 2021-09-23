a2da9de4464a0b8d3479f85521530c51a208be3b
Arê Nascimento <aretuza.gab@gmail.com>
2021-09-23 12:11:42 -0300

    inserindos os metodos

--- src/br/com/zup/Heroi.java
+++ src/br/com/zup/Heroi.java
@@ -24,6 +24,13 @@ public class Heroi {
     public void setVida(double vida) {
         this.vida = vida;
     }
-
+    public void trocarNome() {
+        String nome = this.getNome();
+        this.setNome(nome);
+    }
+    public void receberDano(){
+        double dano = this.getVida();
+        this.setVida(vida);
+    }
 
 }
